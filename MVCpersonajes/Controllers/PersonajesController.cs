fc2960b867a8f20cb97b6585ff5571d2467427f1
Javier Roca <javierroca.dev@gmail.com>
2024-04-24 13:03:15 +0200

    Add project files.

---
+++ MVCpersonajes/Controllers/PersonajesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCpersonajes.Controllers
+{
+    public class PersonajesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult PersonajesAjax()
+        {
+            return View();
+        }
+    }
+}
